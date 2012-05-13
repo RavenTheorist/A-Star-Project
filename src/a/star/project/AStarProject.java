9439203c7ea2b7c1a0e301bdf6a5ead644a08032
RavenTheorist <aminekhalsi@hotmail.com>
2012-05-13 22:50:42 +0200

    Graph/Edge/Vertex Implementations added

--- src/a/star/project/AStarProject.java
+++ src/a/star/project/AStarProject.java
@@ -1,12 +1,49 @@
 package a.star.project;
+
+import java.io.*;
 /**
  *
- * @author Mineland
+ * @author Amine Elkhalsi <aminekhalsi@hotmail.com>
  */
 public class AStarProject
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("salut");
+        affiche();
     }
+    public static void affiche() throws IOException
+    {
+		String ligne = "";
+		String fichier = "test.txt";
+		BufferedReader clavier = new BufferedReader(new InputStreamReader(
+				System.in));
+
+		BufferedReader ficTexte;
+		try {
+			ficTexte = new BufferedReader(new FileReader(new File(fichier)));
+			if (ficTexte == null)
+                        {
+				throw new FileNotFoundException("Fichier non trouvé: "
+						+ fichier);
+			}
+			do
+                        {
+				ligne = ficTexte.readLine();
+				if (ligne != null)
+                                {
+					System.out.println(ligne);
+				}
+			} while (ficTexte != null);
+			ficTexte.close();
+			System.out.println("\n");
+		}
+                catch (FileNotFoundException e)
+                {
+			System.out.println(e.getMessage());
+		}
+                catch (IOException e)
+                {
+			System.out.println(e.getMessage());
+		}
+	}
 }
