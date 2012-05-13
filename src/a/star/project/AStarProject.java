24119fa4e986f1ded058fd351f05025524441faf
RavenTheorist <aminekhalsi@hotmail.com>
2012-05-14 01:32:46 +0200

    Prossibility to extract graph from a text file

--- src/a/star/project/AStarProject.java
+++ src/a/star/project/AStarProject.java
@@ -1,49 +1,18 @@
 package a.star.project;
 
-import java.io.*;
+import java.io.IOException;
+
 /**
- *
+ * Main Class
+ * 
  * @author Amine Elkhalsi <aminekhalsi@hotmail.com>
  */
+
 public class AStarProject
 {
     public static void main(String[] args) throws IOException
     {
-        affiche();
+        Graph graph = new Graph("graph1.txt");
+        System.out.println(graph.toString());
     }
-    public static void affiche() throws IOException
-    {
-		String ligne = "";
-		String fichier = "test.txt";
-		BufferedReader clavier = new BufferedReader(new InputStreamReader(
-				System.in));
-
-		BufferedReader ficTexte;
-		try {
-			ficTexte = new BufferedReader(new FileReader(new File(fichier)));
-			if (ficTexte == null)
-                        {
-				throw new FileNotFoundException("Fichier non trouvé: "
-						+ fichier);
-			}
-			do
-                        {
-				ligne = ficTexte.readLine();
-				if (ligne != null)
-                                {
-					System.out.println(ligne);
-				}
-			} while (ficTexte != null);
-			ficTexte.close();
-			System.out.println("\n");
-		}
-                catch (FileNotFoundException e)
-                {
-			System.out.println(e.getMessage());
-		}
-                catch (IOException e)
-                {
-			System.out.println(e.getMessage());
-		}
-	}
 }
