466f57f8c62ce751e49d6efd91747e8773290aa5
RavenTheorist <aminekhalsi@hotmail.com>
2012-05-13 20:18:14 +0200

    Starting A-Star-Project

---
+++ src/a/star/project/AStarProject.java
@@ -0,0 +1,12 @@
+package a.star.project;
+/**
+ *
+ * @author Mineland
+ */
+public class AStarProject
+{
+    public static void main(String[] args)
+    {
+        System.out.println("salut");
+    }
+}
